e33de7af856805ee84c7bb98fd074845472e4adc
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-08-30 19:45:53 +0100

    fix wrapping issue

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,40 +1,43 @@
 <x-layout>
 
-    <h1>Login</h1>
-
-    <div>
-        <form action="{{ route('login') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-
-            {{-- Email --}}
-            <div class="m-5">
-                <label for="email">Email</label>
-                <input type="text" name="email" class="shadow md:shadow-lg ">
-                @error('email')
+    <main>
+        
+        <div>
+            <h1 class="flex justify-center">Login</h1>
+            <form action="{{ route('login') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                
+                {{-- Email --}}
+                <div class="m-5">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" class="shadow md:shadow-lg ">
+                    @error('email')
                     <p>{{ $message }}</p>
-                @enderror
-            </div>
-
-            {{-- Password --}}
-            <div class="m-5">
-                <label for="password">Password</label>
-                <input type="password" name="password" class="shadow md:shadow-lg ">
-                @error('password')
+                    @enderror
+                </div>
+                
+                {{-- Password --}}
+                <div class="m-5">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="shadow md:shadow-lg ">
+                    @error('password')
                     <p>{{ $message }}</p>
-                @enderror
-            </div>
-
-            <dir>
-                <input type="checkbox" name="remember">
-                <label for="remember">Remember me</label>
-            </dir>
-
-            @error('failed')
+                    @enderror
+                </div>
+                
+                <dir>
+                    <input type="checkbox" name="remember">
+                    <label for="remember">Remember me</label>
+                </dir>
+                
+                @error('failed')
                 <p>{{ $message }}</p>
-            @enderror
-
-            {{-- Submit --}}
-            <button class="btn">Login</button>
-        </form>
-    </div>
+                @enderror
+                
+                {{-- Submit --}}
+                <button class="btn">Login</button>
+            </form>
+        </div>
+    
+    </main>
 </x-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,63 +1,65 @@
 <x-layout>
 
-    <h1>Register a new account</h1>
+    <main>
 
-    <div>
-        <form action="{{ route('register') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            {{-- Username --}}
-            <div class="m-5">
-                <label for="username">Username</label>
-                <input type="text" name="username" class="shadow md:shadow-lg ">
-                @error('username')
+        <div>
+            <h1 class="flex justify-center">Register a new account</h1>
+            <form action="{{ route('register') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                {{-- Username --}}
+                <div class="m-5">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" class="shadow md:shadow-lg ">
+                    @error('username')
                     <p>{{ $message }}</p>
-                @enderror
-            </div>
-
-            {{-- Email --}}
-            <div class="m-5">
-                <label for="email">Email</label>
-                <input type="text" name="email" class="shadow md:shadow-lg ">
-                @error('email')
+                    @enderror
+                </div>
+                
+                {{-- Email --}}
+                <div class="m-5">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" class="shadow md:shadow-lg ">
+                    @error('email')
                     <p>{{ $message }}</p>
-                @enderror
-            </div>
-
-            {{-- Password --}}
-            <div class="m-5">
-                <label for="password">Password</label>
-                <input type="password" name="password" class="shadow md:shadow-lg ">
-                @error('password')
+                    @enderror
+                </div>
+                
+                {{-- Password --}}
+                <div class="m-5">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="shadow md:shadow-lg ">
+                    @error('password')
                     <p>{{ $message }}</p>
-                @enderror
-            </div>
-            
-            {{-- Password Confirmation --}}
-            <div class="m-5">
-                <label for="password_confirmation">Password Confirmation</label>
-                <input type="password" name="password_confirmation" class="shadow md:shadow-lg @error('password') @enderror">
-            </div>
-
-            {{-- avatar --}}
-            <div class="m-5">
-                <label for="avatar">Avatar</label>
-                <input type="file" name="avatar">
-                @error('avatar')
+                    @enderror
+                </div>
+                
+                {{-- Password Confirmation --}}
+                <div class="m-5">
+                    <label for="password_confirmation">Password Confirmation</label>
+                    <input type="password" name="password_confirmation" class="shadow md:shadow-lg @error('password') @enderror">
+                </div>
+                
+                {{-- avatar --}}
+                <div class="m-5">
+                    <label for="avatar">Avatar</label>
+                    <input type="file" name="avatar">
+                    @error('avatar')
                     <p>{{ $message }}</p>
-                @enderror
-            </div>
-            
-            {{-- Banner --}}
-            <div class="m-5">
-                <label for="banner">Banner</label>
-                <input type="file" name="banner">
-                @error('banner')
+                    @enderror
+                </div>
+                
+                {{-- Banner --}}
+                <div class="m-5">
+                    <label for="banner">Banner</label>
+                    <input type="file" name="banner">
+                    @error('banner')
                     <p>{{ $message }}</p>
-                @enderror
-            </div>
-
-            {{-- Submit --}}
-            <button class="btn">register</button>
-        </form>
-    </div>
-</x-layout>
+                    @enderror
+                </div>
+                
+                {{-- Submit --}}
+                <button class="btn">register</button>
+            </form>
+        </div>
+    </main>
+    </x-layout>
