b12e9d261b908d4128a32d639ff4aa83441df4f7
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-09-01 01:18:21 +0100

    initial start on style the postCards

--- resources/views/components/postCards.blade.php
+++ resources/views/components/postCards.blade.php
@@ -1,22 +1,28 @@
 @foreach ($posts as $post)
-        <div class="flex flex-wrap box-content p-4 my-2 border-4 rounded-md drop-shadow-sm" style="background-image: url('{{ $post->postsBanner }}'); ; background-size: cover; background-position: center;">
+        <div class="static flex flex-wrap box-content p- my-[25px] border-2 rounded-md bg-white drop-shadow-sm" style="background-image: url('{{ $post->postsBanner }}'); ; background-size: cover; background-position: center;">
             <div class="max-w-[600px]">
 
-                {{--  --}}
-                
-                {{-- Title --}}
-                <div>
-                    <h2 class="pr-2 text-white drop-shadow-sm" style="text-shadow: 1px 1px 2px black;">{{ $post->title }}</h2>
-                </div>
-                
-                {{-- Date of posting --}}
-                <div class="text-xs font-light mb-2 text-white ">
-                    <p style="text-shadow: 1px 1px 2px black;"> {{ $post->created_at->diffForHumans()}} </p>
+
+                {{-- Avatar --}}
+                <div class="absolute top-[-12px] left-[10px]">
+                    <img class="rounded-full max-h-[50px] border-2 mb-5 " src="storage\account_images\avatar\default_avatar.png" alt="">
                 </div>
 
+                {{-- Date of posting --}}
+                <div class=" text-white ml-[70px]">
+                    <p class="h-[20px]" style="text-shadow: 1px 1px 2px black;"> Created by: <b>{{ $post->user->username }}</b></p>
+                    <p class="text-xs font-light" style="text-shadow: 1px 1px 2px black;"> {{ $post->created_at->diffForHumans()}} </p>
+                </div>
+                
+                {{-- Title --}}
+                <div class="mx-3">
+                    <h2 class=" text-white drop-shadow-sm"  style="text-shadow: 1px 1px 2px black;">{{ $post->title }}</h2>
+                </div>
+
+
                 {{-- Body --}}
-                <div class="text-sm text-white">
-                    <span style="text-shadow: 1px 1px 2px black;">{{ Str::words($post->body, 15) }}</span>
+                <div class="text-ellipsis text-sm text-white mx-3 mb-1">
+                    <span class="break-all" style="text-shadow: 1px 1px 2px black;">{{ Str::words($post->body, 15) }}</span>
                     <a href="{{ route('posts.show', $post)}}" class="text-blue-500 ml-1">Read more &rarr;</a>
                 </div>
             </div>
