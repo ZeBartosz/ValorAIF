a219939932efb0d56bc4575ea54ff905ee044d5c
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-09-01 01:03:43 +0100

    added posts belongTo a user

--- app/Models/posts.php
+++ app/Models/posts.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class posts extends Model
 {
@@ -14,5 +15,9 @@ class posts extends Model
         'body',
         'postsBanner'
     ];
+
+    public function user() : BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 
 }
