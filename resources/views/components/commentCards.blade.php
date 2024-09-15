b47b835d355858751fc72f7abb0dffb88af1ffa8
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-09-15 01:56:44 +0100

    added people can go to user's profile from clicking on their names on comments

--- resources/views/components/commentCards.blade.php
+++ resources/views/components/commentCards.blade.php
@@ -1,16 +1,17 @@
 @props(['post'])
 
 @foreach ($post->comments as $comment)
-    <div class="static flex flex-wrap box-content p- my-[25px] border-2 rounded-lg drop-shadow-sm bg-gray-800 bg-opacity-75">
-        <div class="max-w-[600px] min-w-[600px]">
+    <div class="static flex flex-wrap box-content p- my-[25px] border-2 rounded-lg drop-shadow-sm bg-gray-800 bg-opacity-75 ">
+        <div class="max-w-[600px] min-w-[600px] text-white">
             <div class="flex m-1">
-                <h1 class="ml-1"> {{ app\Models\User::Find($comment->comment_user_id)->username }}</h1>
+                <h1 class="ml-2" style="text-shadow: 1px 1px black, -1px -1px black;"> <em><a
+                    href="{{ route('posts.user', $comment->comment_user_id)}}">{{ app\Models\User::Find($comment->comment_user_id)->username }}</a></em></h1>
                 <div class="ml-2 mt-1 text-xs font-light">
-                    <p>{{ $comment->created_at->diffForHumans() }}</p>
+                    <p style="text-shadow: 1px 1px black, -1px -1px black;">{{ $comment->created_at->diffForHumans() }}</p>
                 </div>
             </div>
 
-            <div class="m-2">
+            <div class="m-3">
                 <p>{{ $comment->body }}</p>
             </div>
         </div>
