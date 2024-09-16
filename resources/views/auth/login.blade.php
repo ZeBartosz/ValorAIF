909607cf326be5702dae457349db2597aaca5bfc
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-09-16 19:13:15 +0100

    added styling for login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,38 +1,50 @@
 <x-layout>
-        <div>
-            <h1 class="flex justify-center">Login</h1>
-            <form action="{{ route('login') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                
-                {{-- Email --}}
-                <div class="m-5">
-                    <label for="email">Email</label>
-                    <input type="text" name="email" class="shadow md:shadow-lg ">
-                    @error('email')
-                    <p>{{ $message }}</p>
+
+    <main class="h-screen flex items-center justify-center ">
+
+        <div class="border-2 rounded-lg drop-shadow-sm bg-gray-800 bg-opacity-75">
+
+            <div class="m-4">
+
+
+                <form action="{{ route('login') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="text-white flex items-center justify-center">
+                        <h1 class="">Login</h1>
+                    </div>
+
+                    {{-- Email --}}
+                    <div class="m-5">
+                        <label for="email">Email</label>
+                        <input type="text" name="email" class="shadow md:shadow-lg ">
+                        @error('email')
+                            <p>{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    {{-- Password --}}
+                    <div class="m-5">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" class="shadow md:shadow-lg ">
+                        @error('password')
+                            <p>{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <dir>
+                        <input type="checkbox" name="remember">
+                        <label for="remember">Remember me</label>
+                    </dir>
+
+                    @error('failed')
+                        <p>{{ $message }}</p>
                     @enderror
-                </div>
-                
-                {{-- Password --}}
-                <div class="m-5">
-                    <label for="password">Password</label>
-                    <input type="password" name="password" class="shadow md:shadow-lg ">
-                    @error('password')
-                    <p>{{ $message }}</p>
-                    @enderror
-                </div>
-                
-                <dir>
-                    <input type="checkbox" name="remember">
-                    <label for="remember">Remember me</label>
-                </dir>
-                
-                @error('failed')
-                <p>{{ $message }}</p>
-                @enderror
-                
-                {{-- Submit --}}
-                <button class="btn">Login</button>
-            </form>
+
+                    {{-- Submit --}}
+                    <button class="btn">Login</button>
+                </form>
+            </div>
         </div>
+
+    </main>
 </x-layout>
