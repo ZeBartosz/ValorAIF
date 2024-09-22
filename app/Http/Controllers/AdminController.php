b23440f79987f29d6d214b1a0f4497d8a627b469
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-09-22 13:57:22 +0100

    added the ability to sent posts and users through to dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,18 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
+
 
 class AdminController extends Controller {
     
     
     public function index()
     {
-        return view('auth.adminDashboard');
+        // $users = User::all();
+        $users = DB::table('users')->get();
+        // $posts = Posts::all();
+        $posts = DB::table('posts')->get();
+
+        return view('auth.adminDashboard', ['users' => $users, 'posts' => $posts]);
     }
 
 }
