dda06bc7dd11dc282cef00bd170423d332ee4e11
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-09-22 09:23:35 +0100

    fixed issue with pagination showing on the side of the page and updated colour on text

--- resources/views/user/posts.blade.php
+++ resources/views/user/posts.blade.php
@@ -2,13 +2,13 @@
 
     
     <div>
-        <h1 class="flex justify-center">{{ $user->username }}'s Posts: {{ $posts->total() }}</h1>
+        <h1 class="flex justify-center text-white mt-2">{{ $user->username }}'s Posts: {{ $posts->total() }}</h1>
         @foreach ($posts as $post)
         <x-postCards :post="$post" />
         @endforeach
+        <div>
+            {{ $posts->links() }}
+        </div>
     </div>
  
-    <div>
-        {{ $posts->links() }}
-    </div>
 </x-layout>
