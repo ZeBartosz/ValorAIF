2490cc8b3e17bd5adb860191bd144530b42ceeab
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-09-29 11:05:53 +0100

    comments moved to the right side

--- resources/views/components/commentCards.blade.php
+++ resources/views/components/commentCards.blade.php
@@ -1,18 +1,24 @@
 @props(['post'])
 
 @foreach ($post->comments as $comment)
-    <div class="static flex flex-wrap box-content p- my-[25px] border-2 rounded-lg drop-shadow-sm bg-gray-800 bg-opacity-75 ">
-        <div class="max-w-[600px] min-w-[600px] text-white">
-            <div class="flex m-1">
-                <h2 class="ml-2" style="text-shadow: 1px 1px black, -1px -1px black;"> <em><a
-                    href="{{ route('posts.user', $comment->comment_user_id)}}">{{ app\Models\User::Find($comment->comment_user_id)->username }}</a></em></h1>
-                <div class="ml-2 mt-1 text-xs font-light">
-                    <p style="text-shadow: 1px 1px black, -1px -1px black;">{{ $comment->created_at->diffForHumans() }}</p>
-                </div>
-            </div>
+    <div class="flex flex-wrap flex-row-reverse">
 
-            <div class="m-3">
-                <p>{{ $comment->body }}</p>
+        <div
+            class="static flex flex-wrap box-content mb-[25px] border-2 rounded-lg drop-shadow-sm bg-gray-800 bg-opacity-75 max-w-[600px] min-w-[600px] ">
+            <div class=" text-white">
+                <div class="flex m-1">
+                    <h2 class="ml-2" style="text-shadow: 1px 1px black, -1px -1px black;"> <em><a
+                                href="{{ route('posts.user', $comment->comment_user_id) }}">{{ app\Models\User::Find($comment->comment_user_id)->username }}</a></em>
+                        </h1>
+                        <div class="ml-2 mt-1 text-xs font-light">
+                            <p style="text-shadow: 1px 1px black, -1px -1px black;">
+                                {{ $comment->created_at->diffForHumans() }}</p>
+                        </div>
+                </div>
+
+                <div class="m-3">
+                    <p>{{ $comment->body }}</p>
+                </div>
             </div>
         </div>
     </div>
