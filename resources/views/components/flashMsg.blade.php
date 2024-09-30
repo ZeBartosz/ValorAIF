32d2df531a41e481e5659f2a242da6571f289044
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-09-30 11:52:02 +0100

    added padding

--- resources/views/components/flashMsg.blade.php
+++ resources/views/components/flashMsg.blade.php
@@ -1,3 +1,3 @@
 @props(['msg', 'bg' => 'bg-green-500'])
 
-<p class="mg-2 text-sm font-medium text-whit px-3 py-1 rounded-md {{ $bg }}">{{ $msg }}</p>
+    <p class="mg-2 p-2 text-sm font-mediumpx-3 py-1 rounded-md {{ $bg }}">{{ $msg }}</p>
