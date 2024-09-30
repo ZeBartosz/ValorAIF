e0a4d476562dce308fb099442925c5e89f96d3d3
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-09-30 12:34:04 +0100

    added blue to bg img

--- resources/views/components/flashMsg.blade.php
+++ resources/views/components/flashMsg.blade.php
@@ -1,3 +1,4 @@
 @props(['msg', 'bg' => 'bg-green-500'])
-
+<div class="mb-3">
     <p class="mg-2 p-2 text-sm font-mediumpx-3 py-1 rounded-md {{ $bg }}">{{ $msg }}</p>
+</div>
--- resources/views/components/postCards.blade.php
+++ resources/views/components/postCards.blade.php
@@ -10,11 +10,17 @@
 @endphp
 @props(['post', 'full' => false])
 
-<div class="static flex flex-wrap box-content p- my-[25px] border-2 rounded-lg drop-shadow-sm"
-    style="background-image: url('/storage/{{ $post->postsBanner }}'); background-size: cover; background-position: center; ">
+
+
+<div class="relative flex flex-wrap box-content p- my-[25px] border-2 rounded-lg drop-shadow-sm">
+    {{-- Background image with blur --}}
+    <div class="absolute inset-0 bg-cover bg-center filter blur-[2px]"
+        style="background-image: url('/storage/{{ $post->postsBanner }}'); z-index: -1;">
+    </div>
+
+
+
     <div class="max-w-[600px] min-w-[600px]">
-
-
         {{-- Avatar --}}
         <div class="absolute top-[-12px] left-[10px]">
             <a href="{{ route('posts.user', $post->user) }}"><img class="rounded-full max-h-[50px] border-2 mb-5 "
@@ -24,20 +30,23 @@
 
         {{-- Date and Auther of post --}}
         <div class=" text-white ml-[70px]">
-            <p class="h-[20px]" style="text-shadow: 1px 1px black, -1px -1px black;"><em><a
+            <p class="h-[20px]"><em><a
                         href="{{ route('posts.user', $post->user) }}">{{ $post->user->username }}</a></em></p>
-            <p class="text-xs font-light" style="text-shadow: 1px 1px black, -1px -1px black;">
+            <p class="text-xs font-light">
                 {{ $post->created_at->diffForHumans() }} </p>
         </div>
 
         {{-- Catagory --}}
-        <div class="text-white absolute top-[4px] right-[4px] {{ $categoryColors[$post->catagory] }} border-2 rounded-md bg-opacity-80">
-            <button id="searchCatagory"><h3 id="catagoryInput" class="mx-2">{{ $post->catagory }}</h3> </button>
+        <div
+            class="text-white absolute top-[4px] right-[4px] {{ $categoryColors[$post->catagory] }} border-2 rounded-md bg-opacity-80">
+            <button id="searchCatagory">
+                <h3 id="catagoryInput" class="mx-2">{{ $post->catagory }}</h3>
+            </button>
         </div>
 
         {{-- Title --}}
         <div class="mx-3 mt-2">
-            <h2 class=" text-white drop-shadow-sm " style="text-shadow: 1px 1px black, -1px -1px black;">
+            <h2 class=" text-white drop-shadow-sm ">
                 {{ $post->title }}</h2>
         </div>
 
@@ -45,13 +54,11 @@
         {{-- Body --}}
         @if ($full)
             <div class="text-ellipsis text-base text-white mx-3 my-2">
-                <span class="break-all "
-                    style="text-shadow: 1px 1px black, -1px -1px black;">{{ $post->body }}</span>
+                <span class="break-all">{{ $post->body }}</span>
             </div>
         @else
             <div class="text-ellipsis text-base text-white mx-3 my-2">
-                <span class="break-all "
-                    style="text-shadow: 1px 1px black, -1px -1px black;">{{ Str::limit($post->body, 50) }}</span>
+                <span class="break-all ">{{ Str::limit($post->body, 50) }}</span>
                 <a href="{{ route('posts.show', $post) }}" class="text-blue-500 ml-1 text-sm">Read more &rarr;</a>
             </div>
         @endif
