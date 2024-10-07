a58c336d2dcc4fa7d418bd02f13a649f4d0f88f7
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-10-07 12:47:48 +0100

    updated the on click reply section to be more inline with edit and delete button

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -35,29 +35,29 @@
         @endforeach
 
         <script>
-            const posts = @json($post->id);  // Blade variable passed to JS
-    
+            const posts = @json($post->id); // Blade variable passed to JS
+
             const replyButtons = document.querySelectorAll('.reply');
-            
+
             replyButtons.forEach(item => {
                 item.addEventListener('click', (e) => {
-                    e.preventDefault();  // Prevent default behavior of the link
-    
+                    e.preventDefault(); // Prevent default behavior of the link
+
                     // Get the comment ID from data attribute
                     const comment = item.getAttribute('data-comment-id');
-    
+
                     item.parentElement.insertAdjacentHTML('beforeend', `
-                        <div class="static flex flex-wrap box-content my-[25px] border-2 rounded-lg drop-shadow-sm bg-gray-900 bg-opacity-75 p-3 max-w-[500px] min-w-[500px]">
+                       <div class="relative flex flex-wrap box-content my-[25px] border-2 rounded-lg drop-shadow-sm bg-gray-900 bg-opacity-75 p-3 max-w-[500px] min-w-[500px] top-[11px] right-[-117px]">
                             <form action="/comments/${posts}/${comment}/store" method="POST">
-                                @csrf
-                                <div class="m-3">
-                                    <label for="body">Reply to the comment</label>
-                                    <textarea name="body" cols="70" rows="2" class="shadow md:shadow-lg"></textarea>
-                                    @error('body')
-                                    <p class="text-red-500">{{ $message }}</p>
-                                    @enderror
-                                </div>
-                                <button class="btn text-black">Reply</button>
+                            @csrf
+                            <div class="m-3">
+                                <label for="body">Reply to the comment</label>
+                                <textarea name="body" cols="70" rows="2" class="shadow md:shadow-lg"></textarea>
+                                @error('body')
+                                <p class="text-red-500">{{ $message }}</p>
+                                @enderror
+                            </div>
+                            <button class="btn text-black">Reply</button>
                             </form>
                         </div>
                     `);
